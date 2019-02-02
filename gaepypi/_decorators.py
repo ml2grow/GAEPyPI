0d18284d02c7ecb76c0c7d9c948496c38ecc3cb8
jpazzel <john.pazzelli@loblaw.ca>
2019-02-02 15:22:18 -0500

    Added support for user access roles
    - basic_auth decorator now accepts an optional list of required roles
    - Accounts in config.json file can also specify one or more user roles
    - Required roles are checked against user's roles when endpoint is called

--- gaepypi/_decorators.py
+++ gaepypi/_decorators.py
@@ -24,8 +24,8 @@ def basic_auth(required_roles=None):
     """
     Decorator to require HTTP auth for request handler
     """
-    def decorator_basic_auth(func, *args, **kwargs):
-        def callf(handler):
+    def decorator_basic_auth(func, handler):
+        def callf(*args, **kwargs):
             auth_header = handler.request.headers.get('Authorization')
             if auth_header is None:
                 __basic_login(handler)
